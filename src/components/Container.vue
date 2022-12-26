4b460e9349b745e3bf175f6c659913d864b088c0
deer-li <1597493814@qq.com>
2022-12-26 17:31:23 +0800

    2022-12-26

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -68,8 +68,6 @@
 
           <el-container class="center-container" direction="vertical">
             <el-header class="btn-bar" style="height: 45px;">
-              <slot name="action">
-              </slot>
               <!--<el-button v-if="upload" type="text" size="medium" icon="el-icon-upload2" @click="handleUpload">{{$t('fm.actions.import')}}</el-button>-->
               <el-button v-if="clearable" type="text" size="medium" icon="el-icon-delete" @click="handleClear">{{$t('fm.actions.clear')}}</el-button>
               <el-button v-if="preview" type="text" size="medium" icon="el-icon-view" @click="handlePreview">{{$t('fm.actions.preview')}}</el-button>
